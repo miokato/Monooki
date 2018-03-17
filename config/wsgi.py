012298d179321c6b43d1683953fa8052a3cd6adf
miokato <miokato@me.com>
2018-03-17 17:52:05 +0900

    Fix

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,11 +8,11 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise import WhiteNoise
+from whitenoise.django import DjangoWhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root='/app/staticfiles')
+application = DjangoWhiteNoise(application)
