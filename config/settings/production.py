2f859a1446d83fb99de349c6c771f40368561cc7
miokato <miokato@me.com>
2018-03-17 21:30:56 +0900

    Fix ALLOWED_HOSTS

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,6 +3,6 @@ import os
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = False
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['monooki.herokuapp.com']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
