4fa33c5cb31e0ca1117cc5bf98d6c48217f04e3d
miokato <miokato@me.com>
2018-03-17 17:58:35 +0900

    Fix

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 import os
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
