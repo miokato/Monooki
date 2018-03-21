c4c648a5b07fb41a2e71f1f785d05f7aae51493b
miokato <miokato@me.com>
2018-03-21 22:36:00 +0900

    Add pub_date to news models

--- news/models.py
+++ news/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class News(models.Model):
     title = models.CharField(max_length=255)
     url = models.URLField(max_length=255)
+    pub_date = models.DateTimeField(null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
