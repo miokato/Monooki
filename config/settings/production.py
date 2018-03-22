1b3c71409a182383b95b7fcc3f60d66dc4868782
miokato <miokato@me.com>
2018-03-22 11:31:07 +0900

    Update allowed hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,6 +3,11 @@ import os
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = False
-ALLOWED_HOSTS = ['monooki.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = [
+    'utomica.co',
+    'www.utomica.co',
+    'monooki.herokuapp.com',
+    'localhost'
+]
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
