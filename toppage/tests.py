647eb3c15ed4c5d42a5729135e239ef90a9884d7
miokato <miokato@me.com>
2018-03-22 23:18:40 +0900

    Add bs4 and test top_page

--- toppage/tests.py
+++ toppage/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from bs4 import BeautifulSoup
 
-# Create your tests here.
+
+class DomTestMixin:
+
+    def soup(self, res):
+        return BeautifulSoup(res.content, 'html.parser')
+
+
+class TopPageTest(TestCase, DomTestMixin):
+
+    def setUp(self):
+        pass
+
+    def assert_title(self, soup, expected):
+        self.assertEqual(soup.select('title')[0].text, expected)
+
+    def test_top_page(self):
+        res = self.client.get('/')
+        self.assertEqual(res.status_code, 200)
+
+        soup = self.soup(res)
+        self.assert_title(soup, 'Utomica')
+
+
+
