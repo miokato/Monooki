2de5d9068db5730105f6bb526dcaa2131430d7ba
miokato <miokato@me.com>
2018-03-22 22:49:14 +0900

    Create test environment with coverage.py and factory-boy

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,27 @@
+import factory
+
 from django.test import TestCase
 
-# Create your tests here.
+from .models import News
+
+
+class NewsFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = News
+
+    title = 'ジャンプに載りました。'
+    url = 'example.com'
+    pub_date = '2018-03-14'
+
+
+class TestNews(TestCase):
+    def setUp(self):
+        self.news = NewsFactory.create()
+
+    def test_collect_field_name(self):
+        self.assertEqual(self.news.title, 'ジャンプに載りました。')
+        self.assertEqual(self.news.url, 'example.com')
+        self.assertEqual(self.news.pub_date, '2018-03-14')
+
+    def test_save_object(self):
+        self.assertEqual(News.objects.count(), 1)
