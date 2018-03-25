625f4c846042ae1b16d1495ecf9fc830aa22ce87
miokato <miokato@me.com>
2018-03-25 12:09:30 +0900

    Add three.js example

--- apps/unity/urls.py
+++ apps/unity/urls.py
@@ -4,4 +4,5 @@ from django.views.generic import TemplateView
 app_name = 'game'
 urlpatterns = (
     path('tutorial/', TemplateView.as_view(template_name='game/tutorial.html'), name='first'),
+    path('balls/', TemplateView.as_view(template_name='game/balls.html'), name='balls'),
 )
