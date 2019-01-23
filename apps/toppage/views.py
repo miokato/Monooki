4278076f4d60d026b4b03401240ba2954b7b1ad2
miokato <miokato@me.com>
2019-01-23 12:23:11 +0900

    トップページの並び替えを公開順にする

--- apps/toppage/views.py
+++ apps/toppage/views.py
@@ -8,7 +8,7 @@ class ItemListView(ListView):
     template_name = 'toppage/home.html'
     model = Item
     context_object_name = 'items'
-    ordering = ['-updated_at']
+    ordering = ['-published_at']
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
